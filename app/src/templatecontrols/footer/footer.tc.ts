c214c39748479dafea47275978abed6f064c36d7
tpwidman <tpwidman@gmail.com>
2015-11-05 10:41:21 -0600

    still stupid

--- app/src/templatecontrols/footer/footer.tc.ts
+++ app/src/templatecontrols/footer/footer.tc.ts
@@ -1,7 +1,19 @@
-import {register, ui} from 'platypus';
+import {controls, events, register, ui, web} from 'platypus';
+import BaseViewControl from '../../viewcontrols/base/base.vc';
+import HomeViewControl from '../../viewcontrols/home/home.vc';
+import AboutViewControl from '../../viewcontrols/lists/lists.vc';
+import ConnectViewControl from '../../viewcontrols/settings/settings.vc';
+import PortfolioViewControl from '../../viewcontrols/tasks/tasks.vc';
 
 export default class FooterTemplateControl extends ui.TemplateControl {
     templateString: string = require('./footer.tc.html');
+    footer: controls.INamedElement<HTMLDivElement, FooterTemplateControl>;
+    
+    context: any = {
+        showFooter:true,
+        homeView: HomeViewControl,
+        
+    }
 }
 
 register.control('footer', FooterTemplateControl);
