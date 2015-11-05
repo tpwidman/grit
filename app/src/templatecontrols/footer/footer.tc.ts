6712a014608ac5fae8c7119e25487b02f3f9c2a0
tpwidman <tpwidman@gmail.com>
2015-11-05 11:41:33 -0600

    making changes

--- app/src/templatecontrols/footer/footer.tc.ts
+++ app/src/templatecontrols/footer/footer.tc.ts
@@ -1,19 +1,9 @@
 import {controls, events, register, ui, web} from 'platypus';
-import BaseViewControl from '../../viewcontrols/base/base.vc';
-import HomeViewControl from '../../viewcontrols/home/home.vc';
-import AboutViewControl from '../../viewcontrols/lists/lists.vc';
-import ConnectViewControl from '../../viewcontrols/settings/settings.vc';
-import PortfolioViewControl from '../../viewcontrols/tasks/tasks.vc';
 
 export default class FooterTemplateControl extends ui.TemplateControl {
     templateString: string = require('./footer.tc.html');
-    footer: controls.INamedElement<HTMLDivElement, FooterTemplateControl>;
     
-    context: any = {
-        showFooter:true,
-        homeView: HomeViewControl,
-        
+    context: any = {}
     }
-}
 
 register.control('footer', FooterTemplateControl);
