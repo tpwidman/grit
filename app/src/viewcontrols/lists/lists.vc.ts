660169d06ceb276e8994f4d9c3745c2acd85dba9
jacksonrc <jackson.r.carr@gmail.com>
2015-11-09 10:58:55 -0600

    added context in lists.ts

--- app/src/viewcontrols/lists/lists.vc.ts
+++ app/src/viewcontrols/lists/lists.vc.ts
@@ -4,14 +4,12 @@ import BaseViewControl from '../base/base.vc';
 export default class ListsViewControl extends BaseViewControl {
     templateString: string = require('./lists.vc.html');
 
-    context: any = {};
+    context: any = {
+        showTaskDetails: false
+    };
     
     expandTaskDetails() {
-    if(this.context.showTaskDetails === true) {
-            this.context.showTaskDetails = false;
-        } else if(this.context.showTaskDetails === false) {
-            this.context.showTaskDetails = true;
-        }
+        this.context.showTaskDetails = !this.context.showTaskDetails;
     }
 }
 
