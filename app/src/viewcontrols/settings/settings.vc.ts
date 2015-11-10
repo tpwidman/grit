12e4b246ccaf91a6fcb98a67260ff50c50cd897a
tpwidman <tpwidman@gmail.com>
2015-11-10 09:50:40 -0600

    add some dropdowns for settings

--- app/src/viewcontrols/settings/settings.vc.ts
+++ app/src/viewcontrols/settings/settings.vc.ts
@@ -4,7 +4,32 @@ import BaseViewControl from '../base/base.vc';
 export default class SettingsViewControl extends BaseViewControl {
     templateString: string = require('./settings.vc.html');
 
-    context: any = {};
+    context: any = {
+        showCreators:false,
+        showVersion:false,
+    };
+    
+    expandCreators(){
+       if(this.context.showCreators === true) {
+            this.context.showCreators = false;
+        } else if(this.context.showCreators === false) {
+            this.context.showCreators = true;
+        }
+    }
+    expandVersion(){
+        this.context.showAboutCreators = !this.context.showAboutCreators;
+    }
+    
+    collapseCreators() {
+        if (this.context.showCreators = true) {
+            this.context.showCreators = false;
+        }
+    }
+    collapseVersion() {
+        if (this.context.showCreators = true) {
+            this.context.showCreators = false;
+        }
+    }
 }
 
 register.viewControl('settings-vc', SettingsViewControl);
