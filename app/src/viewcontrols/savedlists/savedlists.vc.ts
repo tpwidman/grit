11ec3c6ccb3a555faeb84a0c14f6e3c4f41f9a27
acttechie55 <actaylor55@gmail.com>
2015-11-10 16:39:17 -0600

    added local storage kinda to saved lists vc

--- app/src/viewcontrols/savedlists/savedlists.vc.ts
+++ app/src/viewcontrols/savedlists/savedlists.vc.ts
@@ -1,10 +1,15 @@
-import {register} from 'platypus';
+import {register, storage} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import ListsViewControl from '../lists/lists.vc';
 
 export default class SavedlistsViewControl extends BaseViewControl {
     templateString: string = require('./savedlists.vc.html');
 
     context: any = {};
-}
 
+    constructor (private storage: storage.LocalStorage) {
+        super()
+    
+
+register.injectable('storage-repo', Repository, [storage.LocalStorage]);
 register.viewControl('savedlists-vc', SavedlistsViewControl);
